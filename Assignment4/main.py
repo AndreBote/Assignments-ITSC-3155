3341b92221e8e438cc1f0405a20a0ba731c90151
AndreBote <andrebote2005@gmail.com>
2025-10-14 18:42:56 -0400

    Fixed method name read_item in Assignment4

--- Assignment4/main.py
+++ Assignment4/main.py
@@ -21,7 +21,7 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_root(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
